44121aa2b74f3bb1cd3098a93440f7818ac8080f
JaywonC <114188320+JaywonC@users.noreply.github.com>
2024-01-17 13:55:17 -0500

    yes
    change

--- Point.java
+++ Point.java
@@ -1,7 +1,21 @@
 public class Point {
-    public static void main(String[] args){
+    private int position;
+    private int point;
+    public Point(){
 
-		System.out.println("Hello, World!");
-	
-	}
+    }
+    public int getPosition(){
+        return position;
+    }
+    public int getPoint(){
+        return point;
+    }
+
+    public int manhattanDistance(Point other){
+        return 5;
+    }
+    public boolean isCollinear(Point p1, Point p2){
+        return true;
+    }
+
 }
